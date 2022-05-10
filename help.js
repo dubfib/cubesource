e3a66095713a2f37afc0c6f5888808da334d6e17
dubfib <81378985+dubfib@users.noreply.github.com>
2022-05-09 19:07:45 -0700

    Update help.js

--- help.js
+++ help.js
@@ -7,8 +7,14 @@ module.exports = async function MessageCommand(client, message, args) {
 
     const embed = new MessageEmbed()
     .setColor("DARK_AQUA")
-    .setTitle('Help Menu')
-    .setDescription(cmd('?help', 'https://github.com/dubfib/cubesource/blob/main/help.js'))
+    .setTitle('Commands List')
+    .setDescription(
+    cmd('?help', 'https://github.com/dubfib/cubesource/blob/main/help.js') + '\n' +
+    cmd('?meme', 'https://github.com/dubfib/cubesource/blob/main/meme.js') + '\n' +
+    cmd('?rps',  'WORK IN PROGRESS') + '\n' +
+    cmd('?shitpost', 'https://github.com/dubfib/cubesource/blob/main/shitpost.js') + '\n' +
+    cmd('?tictactoe', 'https://github.com/dubfib/cubesource/blob/main/tictactoe.js')
+    )
     .setTimestamp()
     .setFooter({ text: 'The Cube'})
     message.reply({ embeds: [embed]})
