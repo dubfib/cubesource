9be133931dc0a6731d717644c2a477b778c3170e
dubfib <81378985+dubfib@users.noreply.github.com>
2022-05-09 18:59:34 -0700

    Create help.js

---
+++ help.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = async function MessageCommand(client, message, args) {
+    function cmd(name, url) {
+        return '`' + name + '`' + ' ' + '[`Source`](' + url + ')';
+    };
+
+    const embed = new MessageEmbed()
+    .setColor("DARK_AQUA")
+    .setTitle('Help Menu')
+    .setDescription(cmd('?help', 'https://github.com/dubfib/cube'))
+    .setTimestamp()
+    .setFooter({ text: 'The Cube'})
+    message.reply({ embeds: [embed]})
+};
