1abcd77e1c48806ad3277c9ffef0a61564f339f2
dubfib <81378985+dubfib@users.noreply.github.com>
2022-05-10 18:12:07 -0700

    Update help.js

--- help.js
+++ help.js
@@ -1,19 +1,20 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = async function MessageCommand(client, message, args) {
-    function cmd(name, url) {
-        return '`' + name + '`' + ' ' + '[`Source`](' + url + ')';
+    function cmd(name, description) {
+        return '`' + name + '`' + ' ' + '`' + description + '`' + ' ' + '[`Source`](' + 'https://github.com/dubfib/cubesource/blob/main/' + name.replace('?', '') + '.js' + ')';
     };
 
     const embed = new MessageEmbed()
     .setColor("DARK_AQUA")
     .setTitle('Commands List')
     .setDescription(
-    cmd('?help', 'https://github.com/dubfib/cubesource/blob/main/help.js') + '\n' +
-    cmd('?meme', 'https://github.com/dubfib/cubesource/blob/main/meme.js') + '\n' +
-    cmd('?rps',  'https://github.com/dubfib/cubesource/blob/main/rps.js') + '\n' +
-    cmd('?shitpost', 'https://github.com/dubfib/cubesource/blob/main/shitpost.js') + '\n' +
-    cmd('?tictactoe', 'https://github.com/dubfib/cubesource/blob/main/tictactoe.js')
+    cmd('?help', 'Get a command list') + '\n' +
+    cmd('?meme', 'Get a random meme') + '\n' +
+    cmd('?rps',  'Play RPS vs AI') + '\n' +
+    cmd('?shitpost', 'Get a random shitpost') + '\n' +
+    cmd('?tictactoe', 'Play TTT vs AI') + '\n' +
+    cmd('?blackjack', 'Play Blackjack vs AI')
     )
     .setTimestamp()
     .setFooter({ text: 'The Cube'})
